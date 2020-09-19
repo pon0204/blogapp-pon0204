21aaa7bfe4dd0957617bd2c677f3485fae3d8eb4
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2020-09-19 13:37:03 +0900

    記事を更新できるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,8 +20,22 @@ def create
     end
     end
 
+def edit
+@article = Article.find(params[:id]) #paramsidで記事のId 記事のidを取得@articleに代入 @はビューに渡すため
+end
+
+def update
+    @article = Article.find(params[:id])   #記事のidを取得する
+   if @article.update(article_params)      #update【値更新】のメソッドがある。フォームの値を指定(params)
+    redirect_to article_path(@article), notice: '更新できました'     #記事のページに飛ぶ
+   else
+    flash.now[:error] = '更新できませんでした' #メッセージの表示
+    render :edit #編集画面に移動
+   end
+end
+
 private
-def article_params
+def article_params  #フォームの入力内容が回ってくる
     params.require(:article).permit(:title, :content)
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: 'articles#index'
+root to: 'articles#index' #記事一覧の表示
 
-resources :articles, only: [:show, :new, :create]
+resources :articles, only: [:show, :new, :create, :edit, :update]  #アークティルパスのアクションの指定 updateでputとpatchのパス追加
 end
+
+# get 記事の表示
+# put 記事の更新
+# post 記事の作成
